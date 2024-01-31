9794ea1371c61c8d714e68db13cd8dc8dc90be85
Becky Gilbert <beckyannegilbert@gmail.com>
2024-01-31 13:03:27 -0800

    add rollup-plguin-serve and rollup-plugin-livereload, and add watch to rollup (npm run build)

--- packages/lookit-initjspsych/rollup.config.mjs
+++ packages/lookit-initjspsych/rollup.config.mjs
@@ -1,3 +1,31 @@
 import { makeRollupConfig } from "@jspsych/config/rollup";
+import livereload from 'rollup-plugin-livereload';
+import serve from 'rollup-plugin-serve';
 
-export default makeRollupConfig("lookitInitJsPsych");
+let rollupConfig = makeRollupConfig("lookitInitJsPsych");
+
+const host = 'localhost';
+const port = 10001; // this needs to change for each package
+const content_dir = 'dist/';
+
+rollupConfig.plugins = [
+    // options: https://github.com/thgh/rollup-plugin-serve/tree/master?tab=readme-ov-file#options
+    serve({
+        contentBase: content_dir,
+        historyApiFallback: true,
+        host: host,
+        port: port,
+        onListening: function (server) {
+            const address = server.address();
+            const host = (address.address === '::' || address.address === '::1') ? 'localhost' : address.address;
+            const protocol = this.https ? 'https' : 'http';
+            console.log(`Server listening at ${protocol}://${host}:${address.port}/`);
+        }
+    }),
+    livereload({
+        watch: content_dir,
+        delay: 300
+      })
+];
+
+export default rollupConfig;
