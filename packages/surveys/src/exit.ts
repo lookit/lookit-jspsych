2e9b691b7fda66848988ddfc62576271606b474e
CJ Green <44074998+okaycj@users.noreply.github.com>
2024-03-27 17:08:56 -0400

    Added privacy and databrary params

--- packages/surveys/src/exit.ts
+++ packages/surveys/src/exit.ts
@@ -1,37 +1,69 @@
 import { ParameterType, TrialType } from "jspsych";
 import SurveyPlugin from "../../../../jsPsych/packages/plugin-survey/dist";
-import newJSON from "./new.json";
 import surveyJSON from "./survey.json";
 import { survey_function } from "./utils";
 
 const info = <const>{
-  name: SurveyPlugin.info.name,
+  ...SurveyPlugin.info,
   parameters: {
     ...SurveyPlugin.info.parameters,
-    new_value: {
+    show_databrary_options: {
+      type: ParameterType.BOOL,
+      default: true,
+      pretty_name: "Show databrary options",
+    },
+    include_withdrawal_example: {
+      type: ParameterType.BOOL,
+      default: true,
+      pretty_name: "Include withdrawal example",
+    },
+    private_level_only: {
+      type: ParameterType.BOOL,
+      default: false,
+      pretty_name: "Private level only",
+    },
+    additional_video_privacy_text: {
       type: ParameterType.STRING,
       default: "",
-      pretty_name: "Validation function",
+      pretty_name: "Additional video privacy text",
     },
   },
 };
 
 type Info = typeof info;
+type Trial = TrialType<Info>;
 
-function updateQuestions(trial: TrialType<Info>) {
-  if (trial.new_value) {
-    surveyJSON.pages[0].elements.push(newJSON);
+function showDatabraryOptions(trial: Trial) {
+  if (!trial.show_databrary_options) {
+    const elements = surveyJSON.pages[0].elements;
+    const idx = elements.findIndex((element) => element.name === "databrary");
+    elements.splice(idx, 1);
   }
+}
+
+function additionalVideoPrivacyText(trial: Trial) {
+  const element = surveyJSON.pages[0].elements.find(
+    (element) => element.name === "privacy",
+  );
+  if (element) {
+    Object.assign(element, {
+      description: trial.additional_video_privacy_text,
+    });
+  }
+}
+
+function surveyParameters(trial: Trial) {
+  showDatabraryOptions(trial);
+  additionalVideoPrivacyText(trial);
   return JSON.stringify(surveyJSON);
 }
 
 export class ExitSurveyPlugin extends SurveyPlugin {
   static info = info;
-  trial(display_element: HTMLElement, trial: TrialType<Info>) {
-    const survey_json = updateQuestions(trial);
+  trial(display_element: HTMLElement, trial: Trial) {
     return super.trial(display_element, {
       ...trial,
-      survey_json,
+      survey_json: surveyParameters(trial),
       survey_function,
       validation_function: () => null,
     });
