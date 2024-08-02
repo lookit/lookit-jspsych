8bcf34d0168dc8cbe2e4262be43d90bb8352df35
Becky Gilbert <beckyannegilbert@gmail.com>
2024-08-02 14:42:19 -0700

    export the Recorder from record package for use in video-config/consent

--- packages/record/src/index.ts
+++ packages/record/src/index.ts
@@ -1,5 +1,6 @@
 import StartRecordPlugin from "./start";
 import StopRecordPlugin from "./stop";
 import TrialRecordExtension from "./trial";
+import Recorder from './recorder';
 
-export default { TrialRecordExtension, StartRecordPlugin, StopRecordPlugin };
+export default { TrialRecordExtension, StartRecordPlugin, StopRecordPlugin, Recorder };
