7643bc109f6bbdcb6b6f6082f66632ed825e7b3b
Becky Gilbert <beckyannegilbert@gmail.com>
2024-09-12 11:02:09 -0700

    remove reference to mic_check file in record rollup config, rollup copy package used for mic check and dependencies

--- packages/record/rollup.config.mjs
+++ packages/record/rollup.config.mjs
@@ -1,4 +1,3 @@
-import copy from "rollup-plugin-copy";
 import dotenv from "rollup-plugin-dotenv";
 import { importAsString } from "rollup-plugin-string-import";
 import { makeRollupConfig } from "../../rollup.mjs";
@@ -10,9 +9,6 @@ export default makeRollupConfig("chsRecord").map((config) => {
       ...config.plugins,
       // Add support for .env files
       dotenv(),
-      copy({
-        targets: [{ src: "src/mic_check.js", dest: "dist" }],
-      }),
       // Add support to import mustache template files as strings
       importAsString({
         include: ["**/*.mustache"],
