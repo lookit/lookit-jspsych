999677ed5a0bda6a879dc079fc078576cc002281
CJ Green <44074998+okaycj@users.noreply.github.com>
2024-10-03 14:28:09 -0400

    Add support for Handlbars within Rollup

--- packages/record/rollup.config.mjs
+++ packages/record/rollup.config.mjs
@@ -1,5 +1,7 @@
 import image from "@rollup/plugin-image";
+import replace from "@rollup/plugin-replace";
 import dotenv from "rollup-plugin-dotenv";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 import { importAsString } from "rollup-plugin-string-import";
 import { makeRollupConfig } from "../../rollup.mjs";
 
@@ -10,11 +12,20 @@ export default makeRollupConfig("chsRecord").map((config) => {
       ...config.plugins,
       // Add support for .env files
       dotenv(),
-      // Add support to import mustache template files as strings
+      // Add support to import mustache, yaml, handlebars files as strings
       importAsString({
-        include: ["**/*.mustache"],
+        include: ["**/*.mustache", "**/*.yaml", "**/*.hbs"],
       }),
+      // Images are converted to base64
       image(),
+      replace({
+        values: {
+          "process.env.NODE_ENV": JSON.stringify("production"),
+        },
+        preventAssignment: true,
+      }),
+      // Handlebars requires node polyfills.
+      nodePolyfills(),
     ],
   };
 });
