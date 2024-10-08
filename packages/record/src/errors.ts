45bf9be3b120974cd1a212e5dbcb89d2c3efc630
CJ Green <44074998+okaycj@users.noreply.github.com>
2024-10-08 12:50:46 -0400

    Error when no translation is available.

--- packages/record/src/errors.ts
+++ packages/record/src/errors.ts
@@ -229,3 +229,15 @@ export class ImageNotFoundError extends Error {
     super(`"${id}" image not found.`);
   }
 }
+
+/** Error throw what specified language isn't found */
+export class TranslationNotFoundError extends Error {
+  /**
+   * This will be thrown when attempting to init i18n
+   *
+   * @param baseName - Language a2code with region
+   */
+  public constructor(baseName: string) {
+    super(`"${baseName}" translation not found.`);
+  }
+}
