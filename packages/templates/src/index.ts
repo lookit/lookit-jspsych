29ffa75fa71d83f4194c2a27aa07092b92bdeaef
CJ Green <44074998+okaycj@users.noreply.github.com>
2024-10-16 10:59:12 -0400

    Add consentDocument function

--- packages/templates/src/index.ts
+++ packages/templates/src/index.ts
@@ -1,1 +1,14 @@
-export default {};
+import Handlebars from "handlebars";
+import consentDocumentTemplate from "../hbs/consent-document.hbs";
+
+/**
+ * Translate, render, and get consent document HTML.
+ *
+ * @returns Consent document HTML
+ */
+const consentDocument = () => {
+  const view = {};
+  return Handlebars.compile(consentDocumentTemplate)(view);
+};
+
+export default { consentDocument };
---
+++ packages/templates/src/string-import.d.ts
@@ -0,0 +1,14 @@
+declare module "*.svg" {
+  const file: string;
+  export default file;
+}
+
+declare module "*.yaml" {
+  const file: string;
+  export default file;
+}
+
+declare module "*.hbs" {
+  const file: string;
+  export default file;
+}
