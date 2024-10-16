4490784175f176bb1932d1c60222612efc749ca2
CJ Green <44074998+okaycj@users.noreply.github.com>
2024-10-16 11:45:16 -0400

    Add hbs and yaml import as strings to Rollup config

--- packages/templates/rollup.config.mjs
+++ packages/templates/rollup.config.mjs
@@ -1,3 +1,20 @@
-import { makeRollupConfig } from "@jspsych/config/rollup";
-// This package name needs to be unique
-export default makeRollupConfig("chsTemplates");
+// import { makeRollupConfig } from "@jspsych/config/rollup";
+// // This package name needs to be unique
+// export default makeRollupConfig("chsTemplates");
+
+import { importAsString } from "rollup-plugin-string-import";
+import { makeRollupConfig } from "../../rollup.mjs";
+
+export default makeRollupConfig("chsTemplates").map((config) => {
+  return {
+    ...config,
+    plugins: [
+      ...config.plugins,
+
+      // Add support to import yaml and handlebars files as strings
+      importAsString({
+        include: ["**/*.yaml", "**/*.hbs"],
+      }),
+    ],
+  };
+});
