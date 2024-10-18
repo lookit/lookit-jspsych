f6b667b449f171f4ba4b2d9d2c96adb0e5ab6cd9
Becky Gilbert <beckyannegilbert@gmail.com>
2024-10-18 13:25:36 -0700

    add private getCurrentPluginName method to trial extension, get plugin name from info, fix types, fix/add tests

--- packages/record/src/types.ts
+++ packages/record/src/types.ts
@@ -1,3 +1,11 @@
+import { JsPsychPlugin, PluginInfo } from "jspsych";
+import { Class } from "type-fest";
+
+export interface jsPsychPluginWithInfo
+  extends Class<JsPsychPlugin<PluginInfo>> {
+  info: PluginInfo;
+}
+
 /**
  * A valid CSS height/width value, which can be a number, a string containing a
  * number with units, or 'auto'.
