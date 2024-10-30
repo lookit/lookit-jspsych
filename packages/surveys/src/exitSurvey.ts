83346c07c99c39a9e617efbd2bc619c9d7d75e82
CJ Green <44074998+okaycj@users.noreply.github.com>
2024-10-30 16:57:21 -0400

    Add locale parameter and move survey text code to templates

--- packages/surveys/src/exitSurvey.ts
+++ packages/surveys/src/exitSurvey.ts
@@ -1,15 +1,16 @@
 import SurveyPlugin from "@jspsych/plugin-survey";
-import { LookitWindow } from "@lookit/data/dist/types";
+import chsTemplates from "@lookit/templates";
 import { ParameterType, TrialType } from "jspsych";
-import { names, surveyJSON } from "./exit_json";
 import { exitSurveyFunction as survey_function } from "./utils";
-
-declare let window: LookitWindow;
 
 const info = <const>{
   ...SurveyPlugin.info,
   parameters: {
     ...SurveyPlugin.info.parameters,
+    locale: {
+      type: ParameterType.STRING,
+      default: "en-us",
+    },
     show_databrary_options: {
       type: ParameterType.BOOL,
       default: true,
@@ -50,101 +51,9 @@ export class ExitSurveyPlugin extends SurveyPlugin {
   public trial(display_element: HTMLElement, trial: TrialType<Info>) {
     super.trial(display_element, {
       ...trial,
-      survey_json: this.surveyParameters(trial),
+      survey_json: chsTemplates.exitSurvey(trial),
       survey_function,
     });
-  }
-
-  /**
-   * Process all survey parameter functions.
-   *
-   * @param trial - Info parameters.
-   * @returns Survey JSON.
-   */
-  private surveyParameters(trial: Trial) {
-    [
-      this.showDatabraryOptions,
-      this.includeWithdrawalExample,
-      this.additionalVideoPrivacyText,
-      this.privateLevelOnly,
-    ].map((fn) => fn(trial));
-    return surveyJSON;
-  }
-
-  /**
-   * Alter survey to only show "private" on use of media question.
-   *
-   * @param trial - Info parameters.
-   */
-  private privateLevelOnly(trial: Trial) {
-    if (trial.private_level_only) {
-      const media_use_element = surveyJSON.pages[0].elements.find(
-        (element) => element.name === names.useOfMedia,
-      );
-      media_use_element &&
-        Object.assign(media_use_element, {
-          defaultValue: "private",
-          description:
-            "Your video data is private and may only be viewed by authorized scientists.",
-          choicesVisibleIf: "false", // this must be a string expression
-          isRequired: false,
-        });
-    }
-  }
-
-  /**
-   * Alter survey to show Databrary options.
-   *
-   * @param trial - Info parameters.
-   */
-  private showDatabraryOptions(trial: Trial) {
-    if (!trial.show_databrary_options) {
-      const survey_elements = surveyJSON.pages[0].elements;
-      const databrary_share_element_idx = survey_elements.findIndex(
-        (element) => element.name === names.databraryShare,
-      );
-      survey_elements.splice(databrary_share_element_idx, 1);
-    }
-  }
-
-  /**
-   * Alter survey to contain additional video privacy text.
-   *
-   * @param trial - Info parameters.
-   */
-  private additionalVideoPrivacyText(trial: Trial) {
-    const element = surveyJSON.pages[0].elements.find(
-      (element) => element.name === names.useOfMedia,
-    );
-    element &&
-      Object.assign(element, {
-        description: trial.additional_video_privacy_text,
-      });
-  }
-
-  /**
-   * Include parenthetical example in withdrawal language.
-   *
-   * @param trial - Info parameters.
-   */
-  private includeWithdrawalExample(trial: Trial) {
-    const study = window.chs.study;
-    const withdrawal_element = surveyJSON.pages[0].elements.find(
-      (element) => element.name === names.withdrawal,
-    );
-    const example = trial.include_withdrawal_example
-      ? " (your spouse was discussing state secrets in the background, etc.)"
-      : "";
-
-    withdrawal_element &&
-      Object.assign(withdrawal_element, {
-        choices: [
-          {
-            text: `Every video helps us, even if something went wrong! However, if you need your video deleted${example}, check here to completely withdraw your video data from this session from the study. Only your consent video will be retained and it may only be viewed by Lookit project staff and researchers working with ${study.attributes.contact_info} on the study "${study.attributes.name}"; other video will be deleted without viewing.`,
-            value: true,
-          },
-        ],
-      });
   }
 
   /**
---
+++ packages/templates/src/exitSurveyTemplate.ts
@@ -0,0 +1,180 @@
+import { LookitWindow } from "@lookit/data/dist/types";
+import i18next from "i18next";
+import { PluginInfo, TrialType } from "jspsych";
+import { setLocale } from "./utils";
+
+declare const window: LookitWindow;
+
+export const names = <const>{
+  birthDate: "birthDate",
+  databraryShare: "databraryShare",
+  useOfMedia: "useOfMedia",
+  withdrawal: "withdrawal",
+  feedback: "feedback",
+};
+
+export const surveyJSON = {
+  pages: [
+    {
+      elements: [
+        {
+          description: "exp-lookit-exit-survey.why-birthdate",
+          inputType: "date",
+          isRequired: true,
+          maxValueExpression: "today()",
+          name: names.birthDate,
+          title: "exp-lookit-exit-survey.confirm-birthdate",
+          type: "text",
+        },
+        {
+          description: "exp-lookit-exit-survey.databrary-info",
+          enableIf: "({withdrawal} empty) or ({withdrawal.length} = 0)",
+          isRequired: true,
+          name: names.databraryShare,
+          title: "exp-lookit-exit-survey.q-databrary",
+          type: "boolean",
+          valueFalse: "no",
+          valueTrue: "yes",
+        },
+        {
+          choices: [
+            {
+              text: "exp-lookit-exit-survey.private-option-part-1",
+              value: "private",
+            },
+            {
+              text: "exp-lookit-exit-survey.scientific-option",
+              value: "scientific",
+            },
+            {
+              text: "exp-lookit-exit-survey.publicity-option",
+              value: "public",
+            },
+          ],
+          description: "",
+          enableIf: "({withdrawal} empty) or ({withdrawal.length} = 0)",
+          isRequired: true,
+          name: names.useOfMedia,
+          title: "exp-lookit-exit-survey.acceptable-use-header",
+          type: "radiogroup",
+        },
+        {
+          choices: [
+            { text: "exp-lookit-exit-survey.withdrawal-details", value: true },
+          ],
+          defaultValue: [],
+          isRequired: false,
+          name: names.withdrawal,
+          title: "exp-lookit-exit-survey.withdrawal-header",
+          type: "checkbox",
+        },
+        {
+          autoGrow: true,
+          name: names.feedback,
+          rows: 3,
+          title: "exp-lookit-exit-survey.feedback-label",
+          type: "comment",
+        },
+      ],
+      name: "page1",
+    },
+  ],
+  showQuestionNumbers: "off",
+};
+
+/**
+ * Alter survey to only show "private" on use of media question.
+ *
+ * @param trial - Info parameters.
+ */
+const privateLevelOnly = (trial: TrialType<PluginInfo>) => {
+  if (trial.private_level_only) {
+    const media_use_element = surveyJSON.pages[0].elements.find(
+      (element) => element.name === names.useOfMedia,
+    );
+    media_use_element &&
+      Object.assign(media_use_element, {
+        defaultValue: "private",
+        description: "exp-lookit-exit-survey.private-option-part-1",
+        choicesVisibleIf: "false", // this must be a string expression
+        isRequired: false,
+      });
+  }
+};
+
+/**
+ * Alter survey to show Databrary options.
+ *
+ * @param trial - Info parameters.
+ */
+const showDatabraryOptions = (trial: TrialType<PluginInfo>) => {
+  if (!trial.show_databrary_options) {
+    const survey_elements = surveyJSON.pages[0].elements;
+    const databrary_share_element_idx = survey_elements.findIndex(
+      (element) => element.name === names.databraryShare,
+    );
+    survey_elements.splice(databrary_share_element_idx, 1);
+  }
+};
+
+/**
+ * Alter survey to contain additional video privacy text.
+ *
+ * @param trial - Info parameters.
+ */
+const additionalVideoPrivacyText = (trial: TrialType<PluginInfo>) => {
+  const element = surveyJSON.pages[0].elements.find(
+    (element) => element.name === names.useOfMedia,
+  );
+  element &&
+    Object.assign(element, {
+      description: trial.additional_video_privacy_text,
+    });
+};
+
+/**
+ * Translate the survey text.
+ *
+ * @param trial - Info parameters.
+ */
+const translation = (trial: TrialType<PluginInfo>) => {
+  const { contact_info, name } = window.chs.study.attributes;
+  const view = {
+    ...trial,
+    include_example: trial.include_withdrawal_example,
+    contact: contact_info,
+    name,
+  };
+
+  surveyJSON.pages[0].elements.forEach((element) => {
+    // Descriptions
+    element.description &&
+      Object.assign(element, {
+        description: i18next.t(element.description, view),
+      });
+
+    // Titles
+    Object.assign(element, { title: i18next.t(element.title, view) });
+
+    // Choices
+    element.choices?.forEach((choice) => {
+      Object.assign(choice, { text: i18next.t(choice.text, view) });
+    });
+  });
+};
+
+/**
+ * Translate survey text to desired locale.
+ *
+ * @param trial - Trial data including user supplied parameters.
+ * @returns Survey json
+ */
+export const exitSurvey = (trial: TrialType<PluginInfo>) => {
+  setLocale(trial);
+  showDatabraryOptions(trial);
+  additionalVideoPrivacyText(trial);
+  privateLevelOnly(trial);
+  translation(trial);
+
+  return surveyJSON;
+};
