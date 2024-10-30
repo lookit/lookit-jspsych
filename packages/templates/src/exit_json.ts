1d38787a4fbf73cd1a83220bab104e8f12683545
CJ Green <44074998+okaycj@users.noreply.github.com>
2024-10-30 13:10:16 -0400

    Replace exit survey's text with translation keys

--- packages/templates/src/exit_json.ts
+++ packages/templates/src/exit_json.ts
@@ -11,23 +11,20 @@ export const surveyJSON = {
     {
       elements: [
         {
-          description:
-            "We ask again just to check for typos during registration or accidental selection of a different child at the start of the study.",
+          description: "exp-lookit-exit-survey.why-birthdate",
           inputType: "date",
           isRequired: true,
           maxValueExpression: "today()",
           name: names.birthDate,
-          title: "Please confirm your child's birthdate.",
+          title: "exp-lookit-exit-survey.confirm-birthdate",
           type: "text",
         },
         {
-          description:
-            "Only authorized researchers will have access to information in the library. Researchers who are granted access must agree to maintain confidentiality and not use information for commercial purposes. Data sharing will lead to faster progress in research on human development and behavior. If you have any questions about the data-sharing library, please visit [Databrary](https://nyu.databrary.org/) or email ethics@databrary.org.",
+          description: "exp-lookit-exit-survey.databrary-info",
           enableIf: "({withdrawal} empty) or ({withdrawal.length} = 0)",
           isRequired: true,
           name: names.databraryShare,
-          title:
-            "Would you like to share your video and other data from this session with authorized users of the secure data library Databrary?",
+          title: "exp-lookit-exit-survey.q-databrary",
           type: "boolean",
           valueFalse: "no",
           valueTrue: "yes",
@@ -35,15 +32,15 @@ export const surveyJSON = {
         {
           choices: [
             {
-              text: "**Private**: Video may only be viewed by authorized scientists",
+              text: "exp-lookit-exit-survey.private-option-part-1",
               value: "private",
             },
             {
-              text: "**Scientific and educational**: Video may be shared for scientific or educational purposes. For example, we might show a video clip in a talk at a scientific conference or an undergraduate class about cognitive development, or include an image or video in a scientific paper. In some circumstances, video or images may be available online, for instance as supplemental material in a scientific paper.",
+              text: "exp-lookit-exit-survey.scientific-option",
               value: "scientific",
             },
             {
-              text: "**Publicity**: Please select this option if you'd be excited about seeing your child featured on the Lookit website or in a news article about this study! Your video may be shared for publicity as well as scientific and educational purposes; it will never be used for commercial purposes. Video clips shared may be available online to the general public.",
+              text: "exp-lookit-exit-survey.publicity-option",
               value: "public",
             },
           ],
@@ -51,23 +48,24 @@ export const surveyJSON = {
           enableIf: "({withdrawal} empty) or ({withdrawal.length} = 0)",
           isRequired: true,
           name: names.useOfMedia,
-          title: "Use of video clips and images:",
+          title: "exp-lookit-exit-survey.acceptable-use-header",
           type: "radiogroup",
         },
         {
-          choices: [],
+          choices: [
+            { text: "exp-lookit-exit-survey.withdrawal-details", value: true },
+          ],
           defaultValue: [],
           isRequired: false,
           name: names.withdrawal,
-          title: "Withdrawal of video data",
+          title: "exp-lookit-exit-survey.withdrawal-header",
           type: "checkbox",
         },
         {
           autoGrow: true,
           name: names.feedback,
           rows: 3,
-          title:
-            "How did it go? Do you have any suggestions for improving the study?",
+          title: "exp-lookit-exit-survey.feedback-label",
           type: "comment",
         },
       ],
