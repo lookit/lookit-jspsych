1118fc7891840710c59a0abdf2774105a09f9d3d
Becky Gilbert <beckyannegilbert@gmail.com>
2024-11-01 14:44:22 -0700

    Add locale/translation option for video config and stop plugins (#95)
    * rename video_config files to videoConfig, to match our camelCase convention
    * update lookit-initjspsych version in package-lock
    * move hbs with text out of record and into templates package, add chsTemplates exports and use them in record package
    * replace static text with translation file text
    * move html params from instance vars to exported object, import into tests, fix/add tests
    * add uploading video template tests
    * remove static text for step 1 and replace with translation, break step 1 translations into two parts
    * remove new troubleshooting text with no translations
    * add translation for reload webcam button
    * replace Done! text with a green checkmark
    * remove line in step 3 text about box being filled in
    * move info/error text from plugin to template for translation, use message element IDs in plugin for show/hide logic
    * move locale to new section for params supported by all plugins
    * reorder plugins in docs

---
+++ .changeset/yellow-papayas-shop.md
@@ -0,0 +1,8 @@
+---
+"@lookit/templates": minor
+"@lookit/record": minor
+"@lookit/style": patch
+---
+
+Adds support for translation and an optional locale parameter to the
+VideoConfigPlugin and StopRecordPlugin in the record packages.
--- packages/templates/src/index.ts
+++ packages/templates/src/index.ts
@@ -1,3 +1,5 @@
 import { consentVideo } from "./consentVideoTemplate";
+import { uploadingVideo } from "./uploadingVideoTemplate";
+import { videoConfig } from "./videoConfigTemplate";
 
-export default { consentVideo };
+export default { consentVideo, videoConfig, uploadingVideo };
